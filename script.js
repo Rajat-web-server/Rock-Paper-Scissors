a5733742a6d7255d6eacc61e92ca06092154cff8
Your Name <rajatpandey542@gmail.com>
2025-06-17 23:28:05 +0530

    Scorring for 1 round is completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('Hello World');
 
 let humanScore = 0;
 let computerScore = 0;
+let draw_ = 0;
 
 function computerChoice() {
    let getComputerChoice = Math.random();
@@ -32,33 +33,39 @@ function playRound(computer, human) {
    }
    else if (computer === 'Rock' && human === 'paper') {
       console.log('userChoice won');
-      humanScore+1;
+      humanScore + 1;
+      console.log('human score : ' + humanScore);
    }
    else if (computer === 'Rock' && human === 'scissors') {
       console.log('computerChoice won');
-      computerScore+1;
+      computerScore + 1;
+      console.log('Computer score : ' + computerScore);
    }
    else if (computer === 'Paper' && human === 'scissors') {
       console.log('userChoice won');
-      humanScore+1;
+      humanScore + 1;
+      console.log('human score : ' + humanScore);
    }
    else if (computer === 'Paper' && human === 'paper') {
       console.log('Draw')
    }
    else if (computer === 'Paper' && human === 'rock') {
       console.log('computerChoice won');
-      computerScore+1;
+      computerScore + 1;
+      console.log('Computer score : ' + computerScore);
    }
    else if (computer === 'Scissor' && human === 'scissors') {
       console.log('Draw')
    }
    else if (computer === 'Scissor' && human === 'rock') {
       console.log('userChoice won');
-      humanScore+1;
+      humanScore + 1;
+      console.log('human score : ' + humanScore);
    }
    else if (computer === 'Scissor' && human === 'paper') {
       console.log('computerChoice won');
-      computerScore+1;
+      computerScore + 1;
+      console.log('Computer score : ' + computerScore);
    }
 
 }
